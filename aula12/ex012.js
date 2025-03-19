fa509060ff48eef1a2944fcdef1e337d62c5eff7
Daniel Dias <diasdaniel744@gmail.com>
2025-03-18 21:56:59 -0300

    terceiro commit

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,9 +1,10 @@
-var idade = 67
-console.log (`Você tem idade ${idade} anos.`)
-if(idade<16){
-    console.log('Não Vota')
-}else if (idade<18 || idade > 65){ 
-        console.log('Voto opcional')
-}else if (idade => 18){
-    console.log('Voto obrigatório')
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
+if(hora < 12){
+    console.log('Bom dia!')
+}else if(hora<=18) {
+    console.log('Boa tarde!')
+}else{
+    console.log('Boa Noite!')
 }
