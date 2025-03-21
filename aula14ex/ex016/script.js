21546b934a43cc7705869387b9372af70ee08206
Daniel Dias <diasdaniel744@gmail.com>
2025-03-20 22:40:48 -0300

    adicionando o super contador

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,39 @@
+function contar(){
+    let ini = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('ERRO FALTAM DADOS')
+        res.innerHTML= 'Impossível contar'
+    }else{
+        res.innerHTML= 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+            if(p <= 0){
+                window.alert('Passo inválido! Considerando passo 1')
+                p = 1
+            }
+            if (i < f) { 
+                //contagem crescente
+                for(let c = i; c <= f; c+= p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+             //contagem decrescente
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+
+        }
+       res.innerHTML += `\u{1f3c1}`
+    }
+
+
+
+
+
+}
